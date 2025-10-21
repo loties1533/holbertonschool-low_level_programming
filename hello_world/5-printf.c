a31ea3b9536243de6c2460766751052ce2afd714
loties1533 <alexislaubert@yahoo.fr>
2025-10-21 09:46:58 +0000

    aficher un message avec fonction printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entrer du programme
+ *
+ * affiche la phrase dans le printf
+ *
+ * return: 0 programme terminer sans probleme
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0); /* Le programme se termine avec la valeur 0 */
+}
