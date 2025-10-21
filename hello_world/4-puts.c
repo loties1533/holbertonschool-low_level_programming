ed7ec75e878428eec8d8eafa15334a89cf5eaebe
loties1533 <alexislaubert@yahoo.fr>
2025-10-21 09:23:51 +0000

    Ajout du programme 4-puts.c qui affiche : Programming is like building a multilingual puzzle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - point d entrée du programme
+ * main - point d'entree du programme
  *
- * Description ; affiche la phrase
+ * Description: affiche la phrase
  *
- * Return: 0 pour indiquuer que le programme se termine avec succés
- */
+ * Return: 0 pour indiquer que le programme s'est termine avec succes
+*/
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\"");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0); /* Le programme se termine avec la valeur 0 */
 }
