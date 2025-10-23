1c98c773d85b67cf71f13093d8143927b77e11b0
loties1533 <alexislaubert@yahoo.fr>
2025-10-23 14:14:23 +0000

    afficher alphabet avc main.h et main.c

---
+++ functions_nested_loops/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Vérifie le code de la fonction print_alphabet
+ *
+ * Return: Toujours 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * main.h - Contient les prototypes des fonctions pour le projet
+ *
+ * Ce fichier d'en-tête déclare toutes les fonctions utilisées
+ * 
+ */
+
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif /* MAIN_H */
