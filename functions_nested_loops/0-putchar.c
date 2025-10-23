a83b8614814af97111774c7968f8069caa83793e
loties1533 <alexislaubert@yahoo.fr>
2025-10-23 13:42:50 +0000

    ajout README

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Affiche _putchar suivi d'un retour à la ligne
+ * Return: Toujours 0 (succès)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,9 @@
+# Functions and Nested Loops
+
+## Description
+Projet Holberton pour pratiquer :
+- Boucles imbriquées (nested loops)
+- Fonctions et prototypes
+- Portée des variables
+- Fichiers d’en-tête (header files)
+- Compilation avec gcc (-Wall -Werror -Wextra -pedantic -std=gnu89)
