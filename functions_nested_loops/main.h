f5e0b20cbd46be629c77b1dc79726c0c3c8b991e
loties1533 <alexislaubert@yahoo.fr>
2025-10-24 11:49:20 +0000

    affiche le signe d'un nombre

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -8,10 +8,10 @@
  * 
  */
 
+int print_sign(int n);
 int _isalpha(int c);
 int _putchar(char c);
 int _islower(int c );
-void print_alphabet(void);
 void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
