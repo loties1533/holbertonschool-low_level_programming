48da795cd79b9170cb7bca8eabd8726293ae9483
loties1533 <alexislaubert@yahoo.fr>
2025-10-27 19:03:38 +0000

    multiplie 2 entier

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul  multiplie deux entiers
+ * @a: le premier entier
+ * @b: le deuxième entier
+ *
+ * Return: le résultat de la multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,6 +7,7 @@
  * Ce fichier d'en-tête déclare toutes les fonctions utilisées
  * 
  */
+int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
 
