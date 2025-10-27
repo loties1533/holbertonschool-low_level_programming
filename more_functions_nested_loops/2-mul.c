dff9ae1190125789927ceb217b43227a94a13825
loties1533 <alexislaubert@yahoo.fr>
2025-10-27 19:06:45 +0000

    correction betty task 2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * mul  multiplie deux entiers
+ * mul - multiplie deux entiers
+ * Description: Cette fonction prend deux entiers en paramètres
+ * et retourne le résultat de leur multiplication.
  * @a: le premier entier
  * @b: le deuxième entier
  *
