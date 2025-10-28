43d0c4b5e7a2666bf9cb645defeb0259f48ee037
loties1533 <alexislaubert@yahoo.fr>
2025-10-28 13:23:41 +0000

    La fonction affiche '_'la la meme ligne , Si ligne est inférieur ou égal à 0, elle affiche juste un retour à la ligne

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_line - trace une ligne droite dans le terminal
+ * @ligne: nombre de fois que le caractère '_' doit être affiché
+ *
+ * Description: La fonction affiche '_' ligne fois sur la même ligne.
+ * Si ligne est inférieur ou égal à 0, elle affiche juste un retour à la ligne.
+ */
+void print_line(int ligne)
+{
+	int i;
+
+	/* Si ligne est 0 ou négatif, on affiche seulement un retour à la ligne */
+	if (ligne <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	/* Boucle pour afficher '_' ligne fois */
+	for (i = 0; i < ligne; i++)
+	{
+		_putchar('_');
+	}
+
+	/* On termine toujours par un retour à la ligne */
+	_putchar('\n');
+}
