d7b0f6c9201ae34c0c0cac90ddeaeefa891e6370
loties1533 <alexislaubert@yahoo.fr>
2025-10-28 13:31:35 +0000

    correction betty

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -4,14 +4,15 @@
  * print_line - trace une ligne droite dans le terminal
  * @ligne: nombre de fois que le caractère '_' doit être affiché
  *
- * Description: La fonction affiche '_' ligne fois sur la même ligne.
- * Si ligne est inférieur ou égal à 0, elle affiche juste un retour à la ligne.
+ * Description: La fonction affiche '_' ligne fois
+ * sur la même ligne. Si ligne <= 0, elle affiche juste
+ * un retour à la ligne.
  */
 void print_line(int ligne)
 {
 	int i;
 
-	/* Si ligne est 0 ou négatif, on affiche seulement un retour à la ligne */
+	/* Si ligne est 0 ou négatif, affiche seulement un retour à la ligne */
 	if (ligne <= 0)
 	{
 		_putchar('\n');
