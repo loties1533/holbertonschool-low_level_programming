4dafd53a6799753c90e1ef0ce153be54fb8f1255
loties1533 <alexislaubert@yahoo.fr>
2025-10-29 11:55:36 +0000

    remplace la valeurs de n ver un entier 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - point d'entrée du programme
- *
- * Description: Affiche les nombres de 1 à 100,
- * sauf les multiples de 3, 5 et 15 remplacés par
- * "Fizz", "Buzz" ou "FizzBuzz".
- *
- * Return: Always 0
+ * reset_to_98 - met à jour la valeur pointée par n à 98
+ * @n: pointeur vers un entier à modifier
  */
 void reset_to_98(int *n)
+{
+    *n = 98;
+}
