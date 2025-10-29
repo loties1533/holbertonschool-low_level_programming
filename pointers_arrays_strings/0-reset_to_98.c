ac95d52e91d5b0351d1dd49834548cf1c1b60b10
loties1533 <alexislaubert@yahoo.fr>
2025-10-29 10:38:26 +0000

    cration nvx dossier ajout main.h et README.md

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - point d'entrée du programme
+ *
+ * Description: Affiche les nombres de 1 à 100,
+ * sauf les multiples de 3, 5 et 15 remplacés par
+ * "Fizz", "Buzz" ou "FizzBuzz".
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main.h - Contient les prototypes des fonctions pour le projet
+ *
+ * Ce fichier d'en-tête déclare toutes les fonctions utilisées
+ * 
+ */
+void reset_to_98(int *n);
+int _putchar(char c);
+
+#endif /* MAIN_H */
