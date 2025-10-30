8545f7f62f982bfb7cdd73b5619efb15d389f60a
loties1533 <alexislaubert@yahoo.fr>
2025-10-30 11:23:06 +0000

    affiche une chaîne en reverse suivie d'un saut de ligne

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,25 +1,22 @@
 #include "main.h"
+
 /**
  * print_rev - affiche une chaîne en reverse suivie d'un saut de ligne
  * @s: chaîne à afficher
  */
 void print_rev(char *s)
 {
-char *pointeur = s;
-    
-    
-    while (*pointeur != '\0')
-    pointeur++;
-    
-    pointeur--; 
-    
-    
-    while (pointeur >= s)
-    {
-    
-    _putchar(*pointeur);
-    pointeur--;
-    }
-    
-    _putchar('\n');
+	char *pointeur = s;
+
+	while (*pointeur != '\0')
+		pointeur++;
+	pointeur--;
+
+	while (pointeur >= s)
+	{
+		_putchar(*pointeur);
+		pointeur--;
+	}
+
+	_putchar('\n');
 }
