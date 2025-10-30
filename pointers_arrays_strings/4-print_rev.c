994e15066fc4d3a7463b71203c6da50bdc2aa743
loties1533 <alexislaubert@yahoo.fr>
2025-10-30 11:17:53 +0000

    debute task4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - affiche une chaîne en reverse suivie d'un saut de ligne
+ * @s: chaîne à afficher
+ */
+void print_rev(char *s)
+{
+char *pointeur = s;
+    
+    
+    while (*pointeur != '\0')
+    pointeur++;
+    
+    pointeur--; 
+    
+    
+    while (pointeur >= s)
+    {
+    
+    _putchar(*pointeur);
+    pointeur--;
+    }
+    
+    _putchar('\n');
+}
