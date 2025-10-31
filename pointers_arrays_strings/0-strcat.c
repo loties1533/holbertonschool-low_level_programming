a1e1ef4942ef885a95a6bba0a18c1a15a55b9433
loties1533 <alexislaubert@yahoo.fr>
2025-10-31 14:17:20 +0000

    correction betty

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,30 +1,28 @@
 #include "main.h"
 
 /**
-
-- _strcat - concatène la chaîne source à la chaîne de destination
-- @dest : chaîne de destination
-- @src : chaîne source
-- 
-- Retourne : pointeur vers la destination
-*/
+ * _strcat - concatène la chaîne source à la chaîne de destination
+ * @dest: chaîne de destination
+ * @src: chaîne source
+ *
+ * Return: pointeur vers la chaîne de destination
+ */
 char *_strcat(char *dest, char *src)
 {
-int s = 0;
-int d = 0; 
-    
-   
-    while (dest[s] != '\0')
-    s++;
-    
-    while ( src[d] != '\0')
-    {
-      dest[s] = src[d];
-    s++;
-    d++;
-    }
-        dest[s] = '\0';
-    
-     return dest;
+	int s = 0;
+	int d = 0;
 
-    }
+	while (dest[s] != '\0')
+		s++;
+
+	while (src[d] != '\0')
+	{
+		dest[s] = src[d];
+		s++;
+		d++;
+	}
+
+	dest[s] = '\0';
+
+	return (dest);
+}
