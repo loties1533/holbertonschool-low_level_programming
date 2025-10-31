a36ed05c2568148ff2ce09bbd0962d299c5cd7ff
loties1533 <alexislaubert@yahoo.fr>
2025-10-31 22:30:34 +0000

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncat - concatène deux chaînes en utilisant au plus n caractères de src
+ * _strncat - concatène deux chaîne en utilisant au plus n caractère de src
  * @dest: chaîne de destination (où l'on ajoute)
  * @src: chaîne source (ce qu'on ajoute)
  * @n: nombre maximum de caractères à ajouter depuis src
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 	int long_dest = 0;
 	int i = 0;
 
-	while(dest[long_dest] != '\0')
+	while (dest[long_dest] != '\0')
 		long_dest++;
 
 
@@ -23,9 +23,9 @@ char *_strncat(char *dest, char *src, int n)
 		dest[long_dest] = src[i];
 		long_dest++;
 		 i++;
-	 }
+	}
 
-        dest[long_dest] = '\0';
+	dest[long_dest] = '\0';
 
-	return(dest);
+	return (dest);
 }
