79b2e01e0fd074d90f3df41359fc504cca454faa
loties1533 <alexislaubert@yahoo.fr>
2025-11-03 20:06:11 +0000

    trouve un caractere dans une chaine

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - trouve un caractère dans une chaîne
+ * @s: chaîne source
+ * @c: caractère à trouver
+ *
+ * Return: pointeur vers la 1ère occurrence de c, ou 0 si pas trouvé
+ */
+char *_strchr(char *s, char c)
+{
+    while (*s)
+    { 
+        if (*s == c)
+            return (s);
+        s++;
+    }
+
+    if (c == '\0')
+        return (s);
+
+    return (0);
+
+}
