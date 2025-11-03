2ac1cbd10c2ffa75eab8245eeba4c23291823a2f
loties1533 <alexislaubert@yahoo.fr>
2025-11-03 10:59:21 +0000

    remplit une zone mémoire avec un octet constant

---
+++ pointers_arrays_strings2/README.md
@@ -0,0 +1,5 @@
+les pointeurs et la manipulation de mémoire en C,
+
+les tableaux et tableaux multidimensionnels,
+
+les chaînes de caractères et leur manipulation sans utiliser la bibliothèque standard.
---
+++ pointers_arrays_strings2/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main.h - Contient les prototypes des fonctions pour le projet
+ *
+ * Ce fichier d'en-tête déclare toutes les fonctions utilisées
+ * 
+ */
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+
+
+#endif /* MAIN_H */
