98074ec3007fd85e058c213817394ac4611ce4cd
loties1533 <alexislaubert@yahoo.fr>
2025-11-04 18:27:44 +0000

    affiche un echequier en 8*8 avec style betty

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -6,15 +6,14 @@
  */
 void print_chessboard(char (*a)[8])
 {
-    int li , co;
+	int li, co;
 
-    for(li = 0;li < 8; li ++)
-    {
-        for(co = 0; co < 8; co ++ )
-        {
-            _putchar (a[li][co]);
-            
-        }
-        _putchar('\n'); 
-    }
+	for (li = 0; li < 8; li++)
+	{
+		for (co = 0; co < 8; co++)
+		{
+			_putchar(a[li][co]);
+		}
+		_putchar('\n');
+	}
 }
