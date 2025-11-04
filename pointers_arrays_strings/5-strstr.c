27e2b8131512f63bcfe2c31b6f1b8719d2c941c8
loties1533 <alexislaubert@yahoo.fr>
2025-11-04 10:47:26 +0000

    localise la premiere occurence sous chaine needle (chaine haystak)

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strstr - localise la première occurrence de la sous-chaîne needle
+ *            dans la chaîne haystack
+ * @haystack: chaîne principale
+ * @needle: sous-chaîne à rechercher
+ *
+ * Return: pointeur vers le début de la sous-chaîne trouvée,
+ *         ou NULL si needle n'est pas trouvée
+ */
+char *_strstr(char *haystack, char *needle)
+{
+    char *h;
+    char *n;
+
+    if (*needle == '\0')
+
+    return haystack;
+
+    while (haystack)
+    {
+        h = haystack;
+        n = needle;
+
+        while (*n && *h == *n)
+        {
+            h ++;
+            n ++;
+        }
+        if (*n == '\0')
+
+        return (haystack);
+
+        haystack ++;
+    }
+    return 0;
+}
