4bb37154c3c40bac6168dfdf374e5081ff21768e
loties1533 <alexislaubert@yahoo.fr>
2025-11-04 18:42:45 +0000

    style betty task 5 et 6

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,28 +11,26 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-    char *h;
-    char *n;
+	char *h;
+	char *n;
 
-    if (*needle == '\0')
+	if (*needle == '\0')
+		return (haystack);
 
-    return haystack;
+	while (*haystack)
+	{
+		h = haystack;
+		n = needle;
 
-    while (haystack)
-    {
-        h = haystack;
-        n = needle;
+		while (*n && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+			return (haystack);
 
-        while (*n && *h == *n)
-        {
-            h ++;
-            n ++;
-        }
-        if (*n == '\0')
-
-        return (haystack);
-
-        haystack ++;
-    }
-    return 0;
+		haystack++;
+	}
+	return (0);
 }
