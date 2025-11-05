88dc8e757256196ca001997b016e878aa53f1dc7
loties1533 <alexislaubert@yahoo.fr>
2025-11-05 10:05:03 +0000

    task 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
-
-- _puts_recursion - affiche une chaîne de caractères suivie d'un retour à la ligne
-- @s: la chaîne à afficher
-*/
+ * _puts_recursion - affiche une chaîne de caractères suivie d'un retour à la ligne
+ * @s: la chaîne à afficher
+ */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')  
- {
-   _putchar('\n');
-   return;
- }
-    
-    _putchar(*s);        
+    if (*s == '\0')
+    {
+        _putchar('\n');
+        return;
+    }
+
+    _putchar(*s);
     _puts_recursion(s + 1);
 }
---
+++ recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main.h - Contient les prototypes des fonctions pour le projet
+ *
+ * Ce fichier d'en-tête déclare toutes les fonctions utilisées
+ * 
+ */
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+
+#endif /* MAIN_H */
