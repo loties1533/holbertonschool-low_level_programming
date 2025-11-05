5d091af8a9e53e3f8acfe8997712e6c0b4bd1f3d
loties1533 <alexislaubert@yahoo.fr>
2025-11-05 13:13:08 +0000

    correction task 3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -8,14 +8,11 @@
  */
 int factorial(int n)
 {
-    int result = 1;
-    int i;
-
     if (n < 0)
         return (-1);
 
-    for (i = 1; i <= n; i++)
-        result *= i;
+    if (n == 0)
+        return (1);
 
-    return (result);
+    return (n * factorial(n - 1));
 }
