2909ca06e2d9fec59ef99a85d38002e6b27b4add
loties1533 <alexislaubert@yahoo.fr>
2025-11-05 19:57:38 +0000

    verifie si un nombre est premier + fonction test diviseur de n

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * is_prime_number - vérifie si un nombre est premier
+ * @n: nombre à tester
+ *
+ * Return: 1 si premier, sinon 0
+ */
+
+int test_diviseur(int n, int div)
+{
+	if (n% div == 0)
+		return (0);
+	if (div * div > n)
+		return (1);
+	return (test_diviseur(n, div + 1));
+}
+
+
+ int is_prime_number(int n)
+{
+    if (n <= 1)
+    return (0);
+
+    return(test_diviseur(n, 2));
+}
