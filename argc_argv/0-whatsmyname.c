521f85e8ae8400086aa3ecc43f8b21d8bd2997bc
loties1533 <alexislaubert@yahoo.fr>
2025-11-06 12:12:54 +0000

    affiche le nom du prog et remplacer nom avec mmv sans recompiler

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - affiche le nom du programme
+ * @argc: nombre d’arguments (non utilisé)
+ * @argv: tableau des arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+    (void)argc;
+	 printf("%s\n", argv[0]);
+	return (0);
+}
