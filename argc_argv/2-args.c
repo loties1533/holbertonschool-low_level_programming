4f8b1664182543562baa44fc2d88db0bcfd239dc
loties1533 <alexislaubert@yahoo.fr>
2025-11-06 12:52:53 +0000

    affiche les argument ligne par ligne

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - affiche tous les arguments passés au programme
+ * @argc: nombre d'arguments
+ * @argv: tableau des arguments
+ *
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+    int i;
+    for ( i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+    return 0;
+}
