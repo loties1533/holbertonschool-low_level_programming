f18491ae15ccca4adc194ebdffc41fb366640853
loties1533 <alexislaubert@yahoo.fr>
2025-11-07 20:42:30 +0000

    style betty correct t3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * factorial - calcule la factorielle d'un nombre
+ * factorial - retourne la factorielle d'un nombre
  * @n: nombre à calculer
  *
  * Return: factorielle de n, -1 si n < 0
  */
 int factorial(int n)
 {
-    if (n < 0)
-        return (-1);
-
-    if (n == 0)
-        return (1);
-
-    return (n * factorial(n - 1));
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
 }
