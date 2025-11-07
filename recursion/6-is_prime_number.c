eaab993e38bb8ac19d30bbd4373128ee15ecc2a5
loties1533 <alexislaubert@yahoo.fr>
2025-11-07 20:39:09 +0000

    ajout comm 2em fonction T6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * is_prime_number - vérifie si un nombre est premier
+ * test_diviseur - vérifie récursivement si un nombre a un diviseur
  * @n: nombre à tester
+ * @div: diviseur courant à tester
  *
- * Return: 1 si premier, sinon 0
+ * Return: 0 si n n'est pas premier, 1 si n est premier
  */
 int test_diviseur(int n, int div)
 {
@@ -14,6 +15,13 @@ int test_diviseur(int n, int div)
 		return (1);
 	return (test_diviseur(n, div + 1));
 }
+
+/**
+ * is_prime_number - vérifie si un nombre est premier
+ * @n: nombre à tester
+ *
+ * Return: 1 si premier, sinon 0
+ */
 int is_prime_number(int n)
 {
 	if (n <= 1)
