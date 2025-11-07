7db8d06c5dca9bdae130a41c4632ebe0712c5a5b
loties1533 <alexislaubert@yahoo.fr>
2025-11-07 14:05:51 +0000

    style betty strlen

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,10 +6,9 @@
  * Return: longueur de la chaîne
  */
 int _strlen_recursion(char *s)
-{ 
-    if  (*s == '\0')
+{
+	if (*s == '\0')
+		return (0);
 
-        return 0;
-
-    return 1 + _strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 }
