d3a664967a2651e4a349b4fb71ddbe06c8a1a6fe
loties1533 <alexislaubert@yahoo.fr>
2025-11-12 08:31:28 +0000

    ajout main.h readmea t0

---
+++ malloc_free/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main.h - Contient les prototypes des fonctions pour le projet
+ *
+ * Ce fichier d'en-tête déclare toutes les fonctions utilisées
+ * 
+ */
+ 
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+
+#endif /* MAIN_H */
