e4d55fd8430bedd3570c738da1098c2697724b98
loties1533 <alexislaubert@yahoo.fr>
2025-11-13 09:46:03 +0000

    aloue de la memoire ou exit 98

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * malloc_checked - alloue de la mémoire et vérifie le succès
+ * @b: nombre d’octets à allouer
+ *
+ * Retour : pointeur vers la mémoire allouée ou exit(98) si échec
+ */
 
 void *malloc_checked(unsigned int b)
 {
