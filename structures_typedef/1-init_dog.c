7be8e818d80adf7ca2a210b9d3c0c5607d816530
loties1533 <alexislaubert@yahoo.fr>
2025-11-13 15:48:21 +0000

    initialisation structure T1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initialise une variable  type struct dog
+ * @d: pointeur vers la structure à initialiser
+ * @name: nom 
+ * @age: âge 
+ * @owner: propriétaire du chien
+ *
+ * Description: Cette fonction initialise les membres
+ * d'une structure dog avec les valeurs passées en paramètre.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    if (d == NULL)
+        return;
+
+    d->name = name;
+    d->age = age;
+    d->owner = owner;
+}
