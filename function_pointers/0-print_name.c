bdf690147c7ac69a3bc5c71dfdeebb90947ffc34
loties1533 <alexislaubert@yahoo.fr>
2025-11-18 10:57:12 +0000

    t 0 style betty valid

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
 #include "function_pointers.h"
 #include <stdio.h>
+
 /**
- * print_name - affiche un nom avec un un pointeur sur fonction
- * @name: chaîne de caracteres pour le nom
+ * print_name - affiche un nom avec un pointeur sur fonction
+ * @name: chaîne de caractères pour le nom
  * @f: pointeur vers une fonction qui prend un char * et retourne void
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name == NULL || f == NULL )
-        return;
-    
-    f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
