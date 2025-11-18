5cd0afc670159be9a00bc06ff73ecb714dfeb6ba
loties1533 <alexislaubert@yahoo.fr>
2025-11-18 09:38:39 +0000

    fichier.h maj

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
@@ -6,7 +8,8 @@
  *
  * Ce fichier contient les prototypes des fonctions 
  */
+void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 
 
-#endif 
+#endif
