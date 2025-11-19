96cb4f118c3fe4cf7677fa263e1f9b658d5a6fe0
loties1533 <alexislaubert@yahoo.fr>
2025-11-19 16:08:54 +0000

    correction t2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
     for ( i = 0; i < n; i++)
     {
         chaine = va_arg(args, char *);
-        if (chaine ==NULL)
+        if (chaine == NULL)
             printf("nil");
         else
             printf("%s", chaine);
