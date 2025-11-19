587fabaa65faf5718f244f47dcb42ea9423a5a0d
loties1533 <alexislaubert@yahoo.fr>
2025-11-19 12:32:57 +0000

    style betty valider !

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,12 +1,15 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+
 /**
  * variadic_functions.h - Contient le prototype des fonctions variadiques
  *
- * 
- * 
+ * Ce fichier d'en-tête déclare la fonction sum_them_all qui permet
+ * de calculer la somme d'un nombre variable d'entiers.
  */
- /* Prototype d'une fonction qui calcule la somme de tous ses arguments */
+
+/* Prototype d'une fonction qui calcule la somme de tous ses arguments */
 int sum_them_all(const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
+
