de9c681d28b2e668ed440cfd85864939ee6c3b7d
loties1533 <alexislaubert@yahoo.fr>
2025-11-19 12:26:03 +0000

    style betty sur mon.h

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -21,7 +21,7 @@ int sum_them_all(const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		int valeur = va_arg(lst, int);
-		somme += valeur;
+		somme = somme + valeur;
 	}
 	va_end(lst);
 
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,12 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-
-#include <stdarg.h>
-
-/* Function prototype */
+/**
+ * variadic_functions.h - Contient le prototype des fonctions variadiques
+ *
+ * 
+ * 
+ */
+ 
 int sum_them_all(const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
