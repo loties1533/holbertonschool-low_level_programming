8e23835dc52910047b19c06cfdd7d6986f3d8163
loties1533 <alexislaubert@yahoo.fr>
2025-11-19 08:56:42 +0000

    calculatrice T3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,32 @@
+
+#include "3-calc.h"
+
+/* addition */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/* soustraction */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/* multiplication */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/* division */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/* modulo */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
