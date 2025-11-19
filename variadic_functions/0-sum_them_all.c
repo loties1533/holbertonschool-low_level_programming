fcf97c2e228bb6a71c15c4fee3a8c2315c2d8cb8
loties1533 <alexislaubert@yahoo.fr>
2025-11-19 11:29:38 +0000

    betty valid + T0 valide

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,23 +1,29 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 
+/**
+ * sum_them_all - additionne les entiers passés en arguments
+ * @n: nombre d'arguments supplémentaires
+ * @...: les entiers à additionner
+ *
+ * Return: la somme des n entiers, ou 0 si n == 0
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list lst;
-    unsigned int i;
-    int somme = 0;
+	va_list	lst;
+	unsigned int i;
+	int	somme = 0;
 
-    if (n == 0)
-        return 0;
+	if (n == 0)
+		return (0);
 
-        
-    va_start(lst,n);
-    for(i = 0; i < n; i ++)
-    {
-        int valeur = va_arg(lst, int);
-        somme += valeur;
-    }
-    va_end(lst);
+	va_start(lst, n);
+	for (i = 0; i < n; i++)
+	{
+		int valeur = va_arg(lst, int);
+		somme += valeur;
+	}
+	va_end(lst);
 
-    return somme;
+	return (somme);
 }
