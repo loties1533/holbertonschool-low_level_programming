e667fcca5779b9bceff9409a79a8758de576fb9b
loties1533 <alexislaubert@yahoo.fr>
2025-11-21 10:33:11 +0000

    style betty p2 t3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -8,24 +8,27 @@
  */
 static void affiche_char(va_list args)
 {
-    printf ("%c" , va_arg(args, int));
+	printf("%c", va_arg(args, int));
 }
+
 /**
  * affiche_int - affiche un entier
  * @args: liste d'arguments variadiques
  */
 static void affiche_int(va_list args)
 {
-    printf("%d", va_arg(args, int));
+	printf("%d", va_arg(args, int));
 }
+
 /**
  * affiche_float - affiche un float (double)
  * @args: liste d'arguments variadiques
  */
 static void affiche_float(va_list args)
 {
-    printf("%f", va_arg(args , double));
+	printf("%f", va_arg(args, double));
 }
+
 /**
  * affiche_string - affiche une string
  * @args: liste d'arguments variadiques
@@ -33,24 +36,14 @@ static void affiche_float(va_list args)
  */
 static void affiche_string(va_list args)
 {
-    char *s = va_arg(args, char *);
+	char *s = va_arg(args, char *);
 
-    if (s == NULL)
-        s = ("(nil)");
-    
-    printf("%s", s);
+	if (s == NULL)
+		s = "(nil)";
+
+	printf("%s", s);
 }
-/**
- * typedef struct affiche_type - structure : fonction pour chaque type
- * @type_char: caractère représentant le type 
- * @fonction: pointeur vers fonction d'affichage 
- */
-typedef struct affiche_type
-{
 
-    char type_char;
-    void (*fonction)(va_list);
-}affiche_t;
 /**
  * print_all - affiche les arguments selon leur type
  * @format: chaîne représentant les types d'arguments (c,i,f,s)
@@ -58,39 +51,37 @@ typedef struct affiche_type
  */
 void print_all(const char * const format, ...)
 {
-    va_list args;
+	va_list args;
+	affiche_t tableau_affichage[] = {
+		{'c', affiche_char},
+		{'i', affiche_int},
+		{'f', affiche_float},
+		{'s', affiche_string}
+	};
+	unsigned int ind_pos = 0;
+	unsigned int ind_type;
+	char *virgule = "";
 
-    affiche_t tableau_affichage[] = {
-        {'c', affiche_char},
-        {'i', affiche_int},
-        {'f', affiche_float},
-        {'s', affiche_string},
-    };
+	va_start(args, format);
 
-    unsigned int ind_pos = 0;
-    unsigned int ind_type;
-    char *virgule = "";
+	while (format && format[ind_pos])
+	{
+		ind_type = 0;
 
-    va_start(args , format);
+		while (ind_type < 4)
+		{
+			if (format[ind_pos] == tableau_affichage[ind_type].type_char)
+			{
+				printf("%s", virgule);
+				tableau_affichage[ind_type].fonction(args);
+				virgule = ", ";
+				break;
+			}
+			ind_type++;
+		}
+		ind_pos++;
+	}
 
-    while (format && format [ind_pos])
-    {
-        ind_type = 0;
-
-        while (ind_type < 4)
-        {
-            if (format[ind_pos] == tableau_affichage[ind_type].type_char)
-            {
-                printf("%s" , virgule);
-                tableau_affichage[ind_type].fonction(args);
-                virgule = ", ";
-                break;
-            }
-            ind_type++;
-        }
-        ind_pos++;
-    }
-    va_end(args);
-    printf("\n");
-
+	va_end(args);
+	printf("\n");
 }
