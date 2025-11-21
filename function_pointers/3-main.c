35985d082c1931c333f2856d6ac521164cd1445a
loties1533 <alexislaubert@yahoo.fr>
2025-11-21 22:07:27 +0000

    style betty 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,36 +2,42 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
+/**
+ * main - point d'entrée du programme
+ * @argc: nombre d'arguments
+ * @argv: tableau des arguments
+ *
+ * Return:0 = succès sinon codes d'erreur
+ */
 int main(int argc, char *argv[])
 {
-    int a, b, result;
-    int (*op_func)(int, int);
+	int a, b, result;
+	int (*op_func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return 98;
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    a = atoi(argv[1]);
-    b = atoi(argv[3]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
 
-    op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
 
-    if (op_func == NULL)
-    {
-        printf("Error\n");
-        return 99;
-    }
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
 
-    if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
-    {
-        printf("Error\n");
-        return 100;
-    }
+	result = op_func(a, b);
+	printf("%d\n", result);
 
-    result = op_func(a, b);
-    printf("%d\n", result);
-
-    return 0;
+	return (0);
 }
