1b4589fe951c9a5b169e00579537f7932b5524a5
loties1533 <alexislaubert@yahoo.fr>
2025-11-21 21:58:55 +0000

    betty valilde

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,19 +3,9 @@
 
 #include <stddef.h>
 
-/**
- * function_pointers.h - Fichier d'en-tête pour les pointeurs de fonctions
- *
- *  contient les prototypes des fonctions 
- */
-
-/* Recherche un entier dans un tableau */
+/* Prototypes des fonctions pour les pointeurs de fonctions */
 int int_index(int *array, int size, int (*cmp)(int));
-
-/* Applique une fonction à chaque élément d'un tableau */
 void array_iterator(int *array, size_t size, void (*action)(int));
-
-/* Affiche un nom en utilisant une fonction passée en paramètre */
 void print_name(char *name, void (*f)(char *));
 
 #endif /* FUNCTION_POINTERS_H */
