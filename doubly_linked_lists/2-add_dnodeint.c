a630c56a35e04282c9fcc71823db2523aca607fc
loties1533 <alexislaubert@yahoo.fr>
2025-12-05 22:36:15 +0000

    style betty

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,22 +1,29 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * add_dnodeint - ajoute un nœud au début d'une liste doublement chaînée
+ * @head: pointeur vers le pointeur du premier élément de la liste
+ * @n: valeur à ajouter dans le nouveau nœud
+ *
+ * Return: adresse du nouveau nœud, NULL si échec
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *nvx;
+	dlistint_t *nvx;
 
-    nvx = malloc(sizeof(dlistint_t));
-    if (nvx == NULL)
-        return (NULL);
+	nvx = malloc(sizeof(dlistint_t));
+	if (nvx == NULL)
+		return (NULL);
 
-    nvx->n = n;
-    nvx->prev = NULL;
-    nvx->next = *head;
+	nvx->n = n;
+	nvx->prev = NULL;
+	nvx->next = *head;
 
-    if (*head != NULL)
-        (*head)->prev = nvx;
+	if (*head != NULL)
+		(*head)->prev = nvx;
 
-    *head = nvx;
+	*head = nvx;
 
-    return (nvx);
+	return (nvx);
 }
