ca2fdae65cf76a33e86413b4a00adedd6fea8d2c
loties1533 <alexislaubert@yahoo.fr>
2025-12-05 16:16:18 +0000

    t2 a terminer

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+    int fd_fichier;
+    ssize_t nb_caracteres = 0;
+    ssize_t nb_ecrit;
+
+    if (filename == NULL)
+        return (-1);
+
+    if (text_content != NULL) 
+    {
+        while (text_content[nb_caracteres])
+            nb_caracteres++;
+    }
+
+
+    fd_fichier = open(filename, O_WRONLY | O_APPEND);
+    if (fd_fichier == -1)
+        return (-1);
+
+    if (nb_caracteres > 0)
+    {
+        nb_ecrit = write(fd_fichier, text_content, nb_caracteres);
+        if (nb_ecrit != nb_caracteres)
+        {
+            close(fd_fichier);
+
+            return (-1);
+        }
+    }
+
+    if (close(fd_fichier) == -1)
+        return (-1);
+
+    return (1);
+}
