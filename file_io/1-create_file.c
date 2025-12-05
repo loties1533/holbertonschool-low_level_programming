3f6bd2536f5419c052ef5671bf3ed84c45e76e8b
loties1533 <alexislaubert@yahoo.fr>
2025-12-05 22:27:21 +0000

    style betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,37 +3,49 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * create_file - crée un fichier et y écrit du texte
+ * @filename: nom du fichier à créer
+ * @text_content: contenu à écrire dans le fichier
+ *
+ * Return: 1 si succès, -1 en cas d'échec
+ */
 int create_file(const char *filename, char *text_content)
 {
-    int fd_fichier;
-    ssize_t caracteres_a_ecrire = 0;
-    ssize_t caracteres_ecrits;
+	int fd_fichier;
+	ssize_t caracteres_a_ecrire = 0;
+	ssize_t caracteres_ecrits;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[caracteres_a_ecrire])
-            caracteres_a_ecrire++;
-    }
+	if (text_content != NULL)
+	{
+		while (text_content[caracteres_a_ecrire])
+			caracteres_a_ecrire++;
+	}
 
-    fd_fichier = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd_fichier == -1)
-        return (-1);
+	fd_fichier = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd_fichier == -1)
+		return (-1);
 
-    if (caracteres_a_ecrire > 0)
-    {
-        caracteres_ecrits = write(fd_fichier, text_content, caracteres_a_ecrire);
-        if (caracteres_ecrits != caracteres_a_ecrire)
-        {
-            close(fd_fichier);
-            return (-1);
-        }
-    }
+	if (caracteres_a_ecrire > 0)
+	{
+		caracteres_ecrits = write(fd_fichier, text_content, caracteres_a_ecrire);
+		if (caracteres_ecrits != caracteres_a_ecrire)
+		{
+			close(fd_fichier);
+			return (-1);
+		}
+	}
 
-    if (close(fd_fichier) == -1)
-        return (-1);
+	if (close(fd_fichier) == -1)
+		return (-1);
 
-    return (1);
+	return (1);
 }
