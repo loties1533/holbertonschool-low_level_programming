05d4f31a54623e49c545429a12a71ecc69c73a72
loties1533 <alexislaubert@yahoo.fr>
2025-12-05 22:38:44 +0000

    style betty

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,33 +1,39 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * add_dnodeint_end - ajoute un nœud en fin de liste doublement chaînée
+ * @head: pointeur vers le pointeur du premier élément de la liste
+ * @n: valeur à ajouter dans le nouveau noeud
+ *
+ * Return: adresse du nouveau nœud, NULL si échec
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *nvx;
-    dlistint_t *tmp;
+	dlistint_t *nvx;
+	dlistint_t *tmp;
 
-    nvx = malloc(sizeof(dlistint_t));
-    if (nvx == NULL)
-        return (NULL);
+	nvx = malloc(sizeof(dlistint_t));
+	if (nvx == NULL)
+		return (NULL);
 
-    nvx->n = n;
-    nvx->next = NULL;
+	nvx->n = n;
+	nvx->next = NULL;
 
-    if (*head == NULL)
-    {
-        nvx->prev = NULL;
-        *head = nvx;
-        return nvx;
-    }
+	if (*head == NULL)
+	{
+		nvx->prev = NULL;
+		*head = nvx;
+		return (nvx);
+	}
 
-    tmp = *head;
-    
-    while (tmp->next != NULL)
-        tmp = tmp->next;
-   
-   tmp->next = nvx;
-    nvx->prev = tmp;
+	tmp = *head;
 
-    return nvx;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = nvx;
+	nvx->prev = tmp;
+
+	return (nvx);
 }
-
