15e5624d77ea582b5b99900b9453eef1587c39b0
loties1533 <alexislaubert@yahoo.fr>
2025-12-05 13:43:00 +0000

    pour lire un fichier et afficher le contenue sur stdout (fichier texte) plus le main.h

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd_fichier;
+    ssize_t caractere_lu, caractere_ecrit;
+    char *buffer;
+
+    if (filename == NULL)
+        return (0);
+
+    fd_fichier = open(filename, O_RDONLY);
+    if (fd_fichier == -1)
+        return (0);
+
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+    {
+        close(fd_fichier);
+        return (0);
+    }
+
+    caractere_lu = read(fd_fichier, buffer, letters);
+    if (caractere_lu == -1)
+    {
+        free(buffer);
+        close(fd_fichier);
+        return (0);
+    }
+
+    caractere_ecrit = write(STDOUT_FILENO, buffer, caractere_lu);
+
+    free(buffer);
+    close(fd_fichier);
+
+    if (caractere_ecrit == -1 || caractere_ecrit != caractere_lu)
+        return (0);
+
+    return (caractere_ecrit);
+}
