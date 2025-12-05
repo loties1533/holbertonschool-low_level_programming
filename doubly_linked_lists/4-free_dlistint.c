37b906efb1b2ac68c07e5d6810252872ca7c6017
loties1533 <alexislaubert@yahoo.fr>
2025-12-05 22:40:23 +0000

    style betty

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,14 +1,18 @@
-#include "lists.h"
+include "lists.h"
 #include <stdlib.h>
 
+/**
+ * free_dlistint - libère une liste doublement chaînée
+ * @head: pointeur vers le premier élément de la liste
+ */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *tmp;
+	dlistint_t *tmp;
 
-    while (head != NULL)
-    {
-        tmp = head->next;
-        free(head);
-        head = tmp;
-    }
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
 }
